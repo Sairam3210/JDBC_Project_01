5d0d3863b9820ef0207226c21326c909656ee8ab
Sairam3210 <132807092+Sairam3210@users.noreply.github.com>
2025-05-06 09:31:55 +0530

    Update Program_01.java

--- src/com/test/Program_01.java
+++ src/com/test/Program_01.java
@@ -1,9 +1,15 @@
 package com.test;
 
+import java.util.Scanner;
+
 public class Program_01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world..!");
+		System.out.println("Hello World");
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a number:");
+		int a=sc.nextInt();
+		System.out.println("square of"+ a + "is"+(a*a));
 	}
 
 }
