668eeaeeb491fe6c729180e9daae5b4e5d560c5d
konda <konda@KSR.lan>
2025-05-06 09:56:56 +0530

    new commit

--- src/com/test/Program_01.java
+++ src/com/test/Program_01.java
@@ -6,10 +6,10 @@ public class Program_01 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		Scanner sc=new Scanner(System.in);
+		Scanner sc =new Scanner(System.in);
 		System.out.print("Enter a number:");
 		int a=sc.nextInt();
-		System.out.println("square of"+ a + "is"+(a*a));
+		System.out.println("square of " + a + " is " +(a*a));
 	}
 
 }
